367013105eabe994e1f3fe2fbcc9b58fa5b529d8
Tadahiro Ishisaka <ishisaka@gmail.com>
2015-09-12 09:54:59 +0900

    Alpha Release.
     Changes to be committed:
        modified:   .gitignore
    	new file:   CREDITS of Chocolatey
    	new file:   LICENSE(Apache License 2.0)
    	modified:   README.md
    	new file:   src/InstallChocolateyWProxy.ps1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # InstallChocolateyWithProxy
-NTLM認証のあるPROXY環境下でChocolateyをインストールするためのスクリプト
+NTLM認証のあるPROXY環境下でも[Chocolatey](https://chocolatey.org/)のインストールを可能にするPowerShellスクリプトです。
+
+Chocolateyは一度インストールしてしまえば、この手のツールには珍しくNTLM認証のあるProxyでもちゃんと動いてくれますが、初回インストール時に公開されているスクリプトではNTLM認証に阻まれてインストール出来ないという欠点がありました。
+
+このため、このスクリプトではこの残念さを取り除くべく、インストールのスクリプトをProxy対応にしました。
+
+## 使い方
+
+srcディレクトリにあるInstallChocolateyWProxy.ps1を管理者権限で実行してください。
+
+## License
+
+このスクリプトはMITライセンスです。
+
+このスクリプトはChocolateyのソースコードを参照しており、そのライセンス、権利者については以下を参照してください。
+
+* Chocolatey は Apache License 2の元に配布されています。
+* Chocolateyの権利者に関しては、CREDIT of Chocolateyを参照してください。
+
+
+
---
+++ src/InstallChocolateyWProxy.ps1
@@ -0,0 +1,21 @@
+﻿<#
+    Copyright (c) 2015 Tadahiro Ishisaka All rights reserved.
+    This software is released under the MIT License, see LICENSE.txt.
+#>
+$url = 'https://chocolatey.org/install.ps1'
+
+$wc = new-object System.Net.WebClient
+if (!$wc.Proxy.IsBypassed($url)) {
+    $creds = [System.Net.CredentialCache]::DefaultCredentials
+    if ($creds -eq $null) {
+        $cred = get-credential
+        $creds = $cred.GetNetworkCredential();
+    }
+    $proxyaddress = $webclient.Proxy.GetProxy($url).Authority
+    Write-host "Using this proxyserver: $proxyaddress"
+    $proxy = New-Object System.Net.WebProxy($proxyaddress)
+    $proxy.Credentials = $creds
+    $wc.Proxy = $proxy
+}
+
+iex ($wc.DownloadString($url))
